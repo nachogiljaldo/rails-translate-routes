bb087e1842c82d8479c6b787ec67e321197f53dc
Nacho Gil Jaldo <naranja82@gmail.com>
2015-05-02 17:58:52 +0200

    Add support for options in the i18n method.

--- lib/rails-translate-routes.rb
+++ lib/rails-translate-routes.rb
@@ -396,18 +396,25 @@ module ActionDispatch
         def translate &block
           RailsTranslateRoutes.init_with_yield(&block).translate Rails.application.routes
         end
-
+        
         def translate_from_file(file_path, options = {})
           file_path = %w(config locales routes.yml) if file_path.blank?
           r = RailsTranslateRoutes.init_from_file(file_path)
-          r.prefix_on_default_locale = true if options && options[:prefix_on_default_locale] == true
-          r.no_prefixes = true if options && options[:no_prefixes] == true
-          r.keep_untranslated_routes = true if options && options[:keep_untranslated_routes] == true
+          set_options(route, options)
           r.translate Rails.application.routes
         end
 
-        def i18n *locales
-          RailsTranslateRoutes.init_with_i18n(*locales).translate Rails.application.routes
+        def i18n(options = {}, *locales)
+          route = RailsTranslateRoutes.init_with_i18n(*locales)
+          set_options(route, options)
+          route.translate Rails.application.routes
+        end
+
+        private
+        def set_options(route, options)
+          route.prefix_on_default_locale = true if options && options[:prefix_on_default_locale] == true
+          route.no_prefixes = true if options && options[:no_prefixes] == true
+          route.keep_untranslated_routes = true if options && options[:keep_untranslated_routes] == true
         end
       end
     end
